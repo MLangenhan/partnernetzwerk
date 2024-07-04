4d97491edbf3f056497fbf3c331ae9aa0b501fdd
moritz.langenhan <moritz.langenhan@rwth-aachen.de>
2024-07-04 13:50:19 +0200

    fixed a bug where one could post on mobile even if being non-eligible to post

--- Partner-Netzwerk/src/components/shared/Bottombar.tsx
+++ Partner-Netzwerk/src/components/shared/Bottombar.tsx
@@ -1,14 +1,18 @@
 import { bottombarLinks } from '@/constants'; // Import bottombar links from constants file
 import { Link, useLocation } from 'react-router-dom'; // Import Link and useLocation from react-router-dom
+import { useUserContext } from '@/context/AuthContext';
 
 const Bottombar = () => {
   const { pathname } = useLocation(); // Get current pathname from useLocation hook
+  const { user } = useUserContext();
 
   return (
     <section className='bottom-bar'>
       {bottombarLinks.map((link) => {
         const isActive = pathname === link.route; // Check if current pathname matches link route
-
+        if (link.label === 'Beitrag erstellen' && (user.role.includes('Ecurie-Aix Mitglied') || user.role.includes('Ecurie-Aix'))) {
+          return null;
+      }
         return (
           <Link
             to={link.route}
